694608a2aead55c68b39935088b1dcb54ac1e7d3
Lautaro Cozzani <lautaromail@gmail.com>
2014-05-02 18:32:40 +0200

    fix0

--- server/main/config.js
+++ server/main/config.js
@@ -3,15 +3,15 @@ var rest = require("./web/rest");
 
 exports.configure = function(app) {
 	var security = {
-		findById: function(req, res, next) {
-			console.log("SECURITY-FIND_BY_ID");
-			if ( req.params.id == "COCO") {
-				next();	
-			} else {
-				res.send(401,{error:'Operacion no autorizada'});
-			}
+		// findById: function(req, res, next) {
+		// 	console.log("SECURITY-FIND_BY_ID");
+		// 	if ( req.params.id == "COCO") {
+		// 		next();	
+		// 	} else {
+		// 		res.send(401,{error:'Operacion no autorizada'});
+		// 	}
             
-        }
+  //       }
 	};
 	var service = {
 		findAll: function(req, res) {
@@ -27,5 +27,9 @@ exports.configure = function(app) {
             res.send("ok-findById:"+req.params.id);
         }
 	}
+	app.get("/api/test/byuser/", function(req,res) {
+		res.send("BY_USER");
+	});
 	rest.bind('test', service, app, 'api/', security);
+
 };
